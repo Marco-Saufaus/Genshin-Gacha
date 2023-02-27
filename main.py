1c931b9acebfc87f98d88b252e1638ccef7e0e16
Marco Saufaus <marco.saufaus2@gmail.com>
2023-02-27 14:44:03 +0100

    display 5 cards with correct dimensions

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ background = pg.image.load("resources/Background/Eswgy9JUcAM4gte.jpg")
 background = pg.transform.scale(background, RES)
 card_test = pg.image.load("resources/Cards/barbara_gacha_card.webp")
 card_test = pg.transform.scale(card_test, (240, 764))
+card_test2 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test2 = pg.transform.scale(card_test2, (240, 764))
+card_test3 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test3 = pg.transform.scale(card_test3, (240, 764))
+card_test4 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test4 = pg.transform.scale(card_test4, (240, 764))
+card_test5 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test5 = pg.transform.scale(card_test5, (240, 764))
 
 while True:
     for event in pg.event.get():
@@ -20,7 +28,11 @@ while True:
             pg.quit()
             exit()
     screen.blit(background, (0, 0))
-    screen.blit(card_test, (50, 50))
+    screen.blit(card_test, (100, 68))
+    screen.blit(card_test2, (390, 68))
+    screen.blit(card_test3, (680, 68))
+    screen.blit(card_test4, (970, 68))
+    screen.blit(card_test5, (1260, 68))
     pg.display.update()
     clock.tick(FPS)
 
