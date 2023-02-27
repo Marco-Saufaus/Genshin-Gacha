474d8c205aa295eae4246eea84fa8b29cb276e9a
Marco Saufaus <marco.saufaus2@gmail.com>
2023-02-27 14:36:46 +0100

    display card and background

--- main.py
+++ main.py
@@ -9,19 +9,30 @@ screen = pg.display.set_mode(RES)
 pg.display.set_caption("PAY2WIN SIMULATOR")
 clock = pg.time.Clock()
 
-test_surface = pg.Surface((200, 300))
-test_surface.fill("Red")
+background = pg.image.load("resources/Background/Eswgy9JUcAM4gte.jpg")
+background = pg.transform.scale(background, RES)
+card_test = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test = pg.transform.scale(card_test, (240, 764))
+card_test2 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test2 = pg.transform.scale(card_test2, (240, 764))
+card_test3 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test3 = pg.transform.scale(card_test3, (240, 764))
+card_test4 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test4 = pg.transform.scale(card_test4, (240, 764))
+card_test5 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
+card_test5 = pg.transform.scale(card_test5, (240, 764))
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
-
-    for i in range(5):
-        for j in range(2):
-            pg.draw.rect(screen, "Red", [i * 300 + 50, j * 400 + 50, 250, 350], 0, 5)
-
+    screen.blit(background, (0, 0))
+    screen.blit(card_test, (100, 68))
+    screen.blit(card_test2, (390, 68))
+    screen.blit(card_test3, (680, 68))
+    screen.blit(card_test4, (970, 68))
+    screen.blit(card_test5, (1260, 68))
     pg.display.update()
     clock.tick(FPS)
 
