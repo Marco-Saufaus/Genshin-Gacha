1a79c236ebe4bed6e83b725d1f7d3d0952905ca9
Marco Saufaus <Marco.Saufaus2@gmail.com>
2023-02-27 20:15:55 +0100

    Added logic for pulling characters and items. Added image imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 from sys import exit
 import random
-
+from data import *
 from settings import *
 
 pg.init()
@@ -11,16 +11,7 @@ clock = pg.time.Clock()
 
 background = pg.image.load("resources/Background/Eswgy9JUcAM4gte.jpg")
 background = pg.transform.scale(background, RES)
-card_test = pg.image.load("resources/Cards/barbara_gacha_card.webp")
-card_test = pg.transform.scale(card_test, (240, 764))
-card_test2 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
-card_test2 = pg.transform.scale(card_test2, (240, 764))
-card_test3 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
-card_test3 = pg.transform.scale(card_test3, (240, 764))
-card_test4 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
-card_test4 = pg.transform.scale(card_test4, (240, 764))
-card_test5 = pg.image.load("resources/Cards/barbara_gacha_card.webp")
-card_test5 = pg.transform.scale(card_test5, (240, 764))
+
 
 while True:
     for event in pg.event.get():
@@ -28,11 +19,11 @@ while True:
             pg.quit()
             exit()
     screen.blit(background, (0, 0))
-    screen.blit(card_test, (100, 68))
-    screen.blit(card_test2, (390, 68))
-    screen.blit(card_test3, (680, 68))
-    screen.blit(card_test4, (970, 68))
-    screen.blit(card_test5, (1260, 68))
+    screen.blit(Messenger, (100, 68))
+    screen.blit(Sharpshooters_Oath, (390, 68))
+    screen.blit(Recurve_Bow, (680, 68))
+    screen.blit(Slingshot, (970, 68))
+    screen.blit(Ebony_Bow, (1260, 68))
     pg.display.update()
     clock.tick(FPS)
 
