2aa5264fc9fdeec05fedc7b77ca3be3e5fdefb22
Marco Saufaus <Marco.Saufaus2@gmail.com>
2023-02-28 17:54:29 +0100

    Merge testing_random_module into main and clean code

--- data.py
+++ data.py
@@ -1,39 +1,26 @@
 import random
 import pygame as pg
 import os
-
-rarity = ["five star", "four star", "three star"]
-five_star = [100, 200, 300, 400, 500]
-four_star = [10, 20, 30, 40, 50]
-three_star = ["Ebony_Bow", "Messenger", "Raven_Bow", "Recurve_Bow", "Sharpshooters Oath", "Slingshot"]
-
-# category = random.choice(rarity)
-# print(category)
-# character = random.choice(list(category))
-# print(character)
-
-# creates list of 5 random rarity-options from the rarity list
-pulls = random.choices(rarity, weights=(4, 10, 86), k=5)
-
-# pulls items from the itempool depending on rarity
-for pull in pulls:
-    if pull == "five star":
-        print(random.choice(five_star))
-    elif pull == "four star":
-        print(random.choice(four_star))
-    elif pull == "three star":
-        print(random.choice(three_star))
-    else:
-        print(f"{pull} is not valid.")
+from settings import *
 
 
-# if category == "five star":
-#     print(random.choice(five_star))
-# elif category == "four star":
-#     print(random.choice(four_star))
-# else:
-#     print(random.choice(three_star))
+# Rarity Backgrounds
+rarity_5_bg = pg.image.load("resources/Background/orange.png")
+rarity_4_bg = pg.image.load("resources/Background/purple.png")
+rarity_3_bg = pg.image.load("resources/Background/blue.png")
 
+# Characters
+
+# 4-Star
+Barbara = pg.image.load("resources/Cards/Characters/4_Star/barbara_gacha_card.webp")
+Barbara = pg.transform.scale(Barbara, (240, 764))
+
+# 5-Star
+
+
+# Weapons
+
+# 3-Star
 Ebony_Bow = pg.image.load("resources/Cards/Weapons/3_Star/Bow/Ebony_Bow.webp")
 Ebony_Bow = pg.transform.scale(Ebony_Bow, (240, 764))
 Messenger = pg.image.load("resources/Cards/Weapons/3_Star/Bow/Messenger.webp")
@@ -46,5 +33,24 @@ Sharpshooters_Oath = pg.image.load("resources/Cards/Weapons/3_Star/Bow/Sharpshoo
 Sharpshooters_Oath = pg.transform.scale(Sharpshooters_Oath, (240, 764))
 Slingshot = pg.image.load("resources/Cards/Weapons/3_Star/Bow/Slingshot.webp")
 Slingshot = pg.transform.scale(Slingshot, (240, 764))
+
+# 4-Star
+
+
+# 5-Star
+
+# Collection of lists
+rarity = ["five star", "four star", "three star"]
+five_star = [100, 200, 300, 400, 500]
+four_star = [Barbara]
+three_star = [Ebony_Bow, Messenger, Raven_Bow, Recurve_Bow, Sharpshooters_Oath, Slingshot]
+
+# creates list of 5 random rarity-options from the rarity list
+pulls = random.choices(rarity, weights=(0, 10, 90), k=5)
+
+
+# list of card coordinates
+card_coordinates = [(100, 68), (390, 68), (680, 68), (970, 68), (1260, 68)]
+
 
 
--- main.py
+++ main.py
@@ -9,21 +9,31 @@ screen = pg.display.set_mode(RES)
 pg.display.set_caption("PAY2WIN SIMULATOR")
 clock = pg.time.Clock()
 
+
 background = pg.image.load("resources/Background/Eswgy9JUcAM4gte.jpg")
 background = pg.transform.scale(background, RES)
+screen.blit(background, (0, 0))
 
+
+# pulls items from the itempool depending on rarity
+for index, pull in enumerate(pulls):
+    if pull == "five star":
+        screen.blit(rarity_5_bg, card_coordinates[index])
+        screen.blit(random.choice(five_star), card_coordinates[index])
+    elif pull == "four star":
+        screen.blit(rarity_4_bg, card_coordinates[index])
+        screen.blit(random.choice(four_star), card_coordinates[index])
+    elif pull == "three star":
+        screen.blit(rarity_3_bg, card_coordinates[index])
+        screen.blit(random.choice(three_star), card_coordinates[index])
+    else:
+        print(f"{pull} is not valid.")
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
-    screen.blit(background, (0, 0))
-    screen.blit(Messenger, (100, 68))
-    screen.blit(Sharpshooters_Oath, (390, 68))
-    screen.blit(Recurve_Bow, (680, 68))
-    screen.blit(Slingshot, (970, 68))
-    screen.blit(Ebony_Bow, (1260, 68))
     pg.display.update()
     clock.tick(FPS)
 
